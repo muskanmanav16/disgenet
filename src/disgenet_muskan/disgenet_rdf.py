ac336e8db49b314c1fa704ee8aa4664ad7343316
manavm0 <s0mumana@uni-bonn.de>
2023-10-21 17:59:16 +0200

    ttl

--- src/disgenet_muskan/disgenet_rdf.py
+++ src/disgenet_muskan/disgenet_rdf.py
@@ -42,23 +42,17 @@ def create_ttl(ttl_file:str,engine=engine):
 
     Returns:
         str: The serialized RDF data in Turtle format.
-    """    
+    """  
     combined_df,df_snps= makedataframe(engine)
-
-    #Define Classes
+    #define classes
     disease= URIRef(f"http://hgnc.com/#disease")
-    snp = URIRef(f"http://hgnc.com/#snp")
     g=Graph()
-    #Define properties
+    #define properties
     conceptid = URIRef(f"http://hgnc.com/#disease_id") 
     diseasename = URIRef(f"http://hgnc.com/#disease_Name") 
-    snpid_uri=URIRef(f"http://hgnc.com/#snp_id") 
-    chromosome_uri=URIRef(f"http://hgnc.com/#chromosome")
-    position_uri=URIRef(f"http://hgnc.com/#position")
-    score_uri=URIRef(f"http://hgnc.com/#score")
-    #Relations
+    #relation
     has_associated_with=URIRef(f"http://hgnc.com/#HasAssociatedWith") 
-    #Add gene , disease nodes & properties in the graph
+
     for id, props in combined_df.iterrows():
         uri_ref_gene=URIRef(f'https://www.genenames.org/data/gene-symbol-report/#!/symbol/{props.gene_symbol}')
         uri_ref_disease= URIRef(f'https://www.ncbi.nlm.nih.gov/medgen/{props.disease_id}')
@@ -66,8 +60,17 @@ def create_ttl(ttl_file:str,engine=engine):
         g.add((uri_ref_disease,conceptid,Literal(props.disease_id)))
         g.add((uri_ref_disease,diseasename,Literal(props.disease_name)))
         g.add((uri_ref_gene,has_associated_with,uri_ref_disease))
-    #Add Snp nodes & properties in the graph
+    #SNP INFORMATION
+    #define class
+    snp = URIRef(f"http://hgnc.com/#snps")
+    #define properties for snps
+    snpid_uri=URIRef(f"http://hgnc.com/#snp_id") 
+    chromosome_uri=URIRef(f"http://hgnc.com/#chromosome")
+    position_uri=URIRef(f"http://hgnc.com/#position")
+    score_uri=URIRef(f"http://hgnc.com/#score")
+
     for id, props in df_snps.iterrows():
+        uri_ref_disease= URIRef(f'https://www.ncbi.nlm.nih.gov/medgen/{props.disease_id}')
         uri_ref_snp= URIRef(f'http://identifiers.org/dbsnp/{props.snp_id}')
         g.add((uri_ref_snp,RDF.type,snp))
         g.add((uri_ref_snp,snpid_uri,Literal(props.snp_id)))
